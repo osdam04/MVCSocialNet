7d160c6898615af483e392d56cff82c9c933cbd7
Dam <ospanov.dam04@gmail.com>
2025-04-23 23:55:30 +0500

    Revert "Update Create Action parameter from Dami to Damir"
    This reverts commit 3967a8197be339ae1a3b5988d288ae563d62cb04.

--- MVCSocialNet/Controllers/HomeController.cs
+++ MVCSocialNet/Controllers/HomeController.cs
@@ -31,7 +31,7 @@ namespace MVCSocialNet.Controllers
 
         public IActionResult Create()
         {
-            return Content("Damir");
+            return Content("Dami");
         }
     }
 }
