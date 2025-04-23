690578d8b282fe589bdc858e744d0d0a4665c138
Dam <ospanov.dam04@gmail.com>
2025-04-23 17:23:11 +0500

    Add Create Action

--- MVCSocialNet/Controllers/HomeController.cs
+++ MVCSocialNet/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace MVCSocialNet.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Create()
+        {
+            return Content("Dam");
+        }
     }
 }
